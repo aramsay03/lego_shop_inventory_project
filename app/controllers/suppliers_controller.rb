67bdeae6329df18f65486d9c071f7479d8bcd95c
aramsay03 <alan@aramsay.co.uk>
2020-02-11 18:38:19 +0000

    Suppliers editing functionality added to frontend.

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -14,16 +14,25 @@ get '/suppliers/new' do
   erb( :"supplier/new")
 end
 
+# CREATE - CREATE (POST) #
 post '/suppliers' do
   @supplier = Supplier.new(params)
   @supplier.save()
   redirect to "/suppliers"
 end
 
-# CREATE - CREATE (POST) #
-
 # READ - one/show (by id) (GET) #
 
 # UPDATE - (find by id (GET)) & (update (POST)) #
+get '/suppliers/:id/edit' do
+  @supplier = Supplier.find(params[:id])
+  erb( :"supplier/edit")
+end
+
+post '/suppliers/:id' do
+  @supplier = Supplier.new(params)
+  @supplier.update()
+  redirect to "/suppliers"
+end
 
 # DELETE #
